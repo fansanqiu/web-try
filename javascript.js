1e28bbcdedb93b67d6f0acdd07092369c8a996f0
范三球 <58503364+fansanqiu@users.noreply.github.com>
2024-06-16 23:56:08 +0800

    点击按钮后在页面内创建一个p元素练习

--- javascript.js
+++ javascript.js
@@ -1,29 +1,6 @@
-const container = document.querySelector("#container")
+const btn = document.querySelector("#btn")
 
-const redP = document.createElement("p")
-redP.classList.add("redP")
-redP.textContent = "Hey I’m red!"
-redP.style.color = "red"
-container.appendChild(redP)
+const addBtn = document.createElement("p")
+addBtn.textContent = "Hello World"
 
-const blueH3 = document.createElement("h3")
-blueH3.classList.add("blueH3")
-blueH3.textContent = "I’m a blue h3!"
-blueH3.style.color = "blue"
-container.appendChild(blueH3)
-
-const blackDiv = document.createElement("div")
-blackDiv.classList.add("blackDiv")
-blackDiv.style.cssText = "border: solid black; background-color: pink;"
-container.appendChild(blackDiv)
-
-const simpleH1 = document.createElement("h1")
-simpleH1.classList.add("simpleH1")
-simpleH1.textContent = "I’m in a div"
-blackDiv.appendChild(simpleH1)
-
-const simpleP = document.createElement("p")
-simpleP.classList.add("simpleP")
-simpleP.textContent = "ME TOO!"
-blackDiv.appendChild(simpleP)
-
+btn.onclick = () => document.body.appendChild(addBtn)
